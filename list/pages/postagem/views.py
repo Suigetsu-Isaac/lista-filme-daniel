2d5b8a8e59854e06d11a6e8a7adad55848b64662
IsaacSouzaTSI <59900989+Suigetsu-Isaac@users.noreply.github.com>
2024-08-15 13:43:04 -0300

    Update views.py
    criei createPostagem para dividir a parte de listar e criar e restringir a criação para só poder fazer isso se for logado precisa mudar a url

--- list/pages/postagem/views.py
+++ list/pages/postagem/views.py
@@ -2,42 +2,44 @@ from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+
 from list.models import Avaliacao,Filmes,GeneroFilme,PlataformaStreamingFilme,SkinUser
 
 def meta(req):
     return req.META.get('HTTP_REFERER', '/')
+
+@login_required(login_url='/login/')
+def createPostagem(req,pk):
+    comentario = req.POST['comentario']
+    nota = req.POST['nota']
+    user =  SkinUser.objects.get(usuario__username=req.user)  
+    try:
+        ava = Avaliacao.objects.create(comentario=comentario, nota=nota, filme=filme, skin=user)
+    except:
+        context['error'] = 'Avaliação já foi feita'
+    return redirect(meta(req))
+
 def postagem(req,pk):
     filme = Filmes.objects.get(id=pk)
     context = {}
-    if req.method == 'POST':
         
-        comentario = req.POST['comentario']
-        nota = req.POST['nota']
-        user =  SkinUser.objects.get(usuario__username=req.user)  
-        try:
-            ava = Avaliacao.objects.create(comentario=comentario, nota=nota, filme=filme, skin=user)
-        except:
-            context['error'] = 'Avaliação já foi feita'
-        return redirect(meta(req))
-    else:
-        
-        ava = Avaliacao.objects.filter(filme__id=pk)
-        for a in ava:
-            a.like_count = a.likes_dislikes.filter(vote=1).count()
-            a.dislike_count = a.likes_dislikes.filter(vote=-1).count()
-        generos = GeneroFilme.objects.filter(filme__id=pk)
-        plataformas = PlataformaStreamingFilme.objects.filter(filme__id=pk)
-        print(filme)
-        print(ava)
-        print(generos)
-        print(plataformas)
+    ava = Avaliacao.objects.filter(filme__id=pk)
+    for a in ava:
+        a.like_count = a.likes_dislikes.filter(vote=1).count()
+        a.dislike_count = a.likes_dislikes.filter(vote=-1).count()
+    generos = GeneroFilme.objects.filter(filme__id=pk)
+    plataformas = PlataformaStreamingFilme.objects.filter(filme__id=pk)
+    print(filme)
+    print(ava)
+    print(generos)
+    print(plataformas)
 
-        
-        c= {
-            'filme': filme,
-            'avaliacao': ava,
-            'generos' : generos,
-            'plataformas':plataformas
-            }
-        context.update(c)
-        return render(req,'postagem.html',context)
+    
+    c= {
+        'filme': filme,
+        'avaliacao': ava,
+        'generos' : generos,
+        'plataformas':plataformas
+        }
+    context.update(c)
+    return render(req,'postagem.html',context)
